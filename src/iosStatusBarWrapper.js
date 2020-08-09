adce46e20fdc6f79f35b4d9561b6c6fb5756ae99
MadhaviG97 <madhavi.17@cse.mrt.ac.lk>
2020-08-09 09:03:24 +0530

    last merge bug solved

--- src/iosStatusBarWrapper.js
+++ src/iosStatusBarWrapper.js
@@ -11,5 +11,6 @@ const styles = StyleSheet.create({
   statusBarBackground: {
     height: (Platform.OS === 'ios') ? 18 : 0, //this is just to test if the platform is iOS to give it a height of 18, else, no height (Android apps have their own status bar)
     marginTop:(Platform.OS === 'ios') ? 10 : 0,
-
+  },
+});
 module.exports = StatusBarBackground;
